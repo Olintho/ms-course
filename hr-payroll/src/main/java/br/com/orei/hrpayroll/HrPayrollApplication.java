5f88e61dcebefa87364709cbd981efc178a55158
Olintho <olinthojesus@yahoo.com.br>
2022-11-23 22:56:25 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,13 @@ package br.com.orei.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
+// HEREIN : OSJ Capitulo 16 
+
+@RibbonClient(name = "hr-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
