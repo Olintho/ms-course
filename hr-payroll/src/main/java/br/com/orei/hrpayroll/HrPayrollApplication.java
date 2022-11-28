ed0be2f114cd13488ec17fc2fd1eb0382fec8889
Olintho Jesus <olintho.jesus@cnaga.brasil>
2022-11-28 15:55:46 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
@@ -2,13 +2,14 @@ package br.com.orei.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-// HEREIN : OSJ Capitulo 20
+// HEREIN : OSJ Capitulo 21
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
