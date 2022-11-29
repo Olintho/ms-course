42bac680a44f11f9ac62e96a1c14ff044d16fa14
Olintho <olinthojesus@yahoo.com.br>
2022-11-28 23:08:46 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/br/com/orei/hrpayroll/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/orei/hrpayroll/HrConfigServerApplication.java
@@ -2,7 +2,9 @@ package br.com.orei.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
