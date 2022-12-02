eee481fe2aa5080f582e6f05bcd57129892bf445
Olintho <olinthojesus@yahoo.com.br>
2022-12-02 09:43:10 -0300

    hr-worker as a config server client

--- hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/orei/hrpayroll/HrPayrollApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-// HEREIN : OSJ Capitulo 26
+// HEREIN : OSJ Capitulo 28 - Fase 3
+
 
 @EnableCircuitBreaker
 @EnableEurekaClient
