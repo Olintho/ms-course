5e3a715740d729de967e1928d678310453158e99
Olintho <olinthojesus@yahoo.com.br>
2022-12-09 09:25:18 -0300

    Partial commit (vacation)

--- hr-oauth/src/main/java/br/com/orei/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/br/com/orei/hroauth/HrOauthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-// HEREIN : OSJ Capitulo 42
+// HEREIN : OSJ Capitulo 44
 
 @EnableFeignClients
 @EnableEurekaClient
